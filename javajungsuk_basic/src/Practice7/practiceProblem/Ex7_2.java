bad793dd29acb8027f6bef53ac125fbaaefac888
tobe1205@hanmail.net <tobe1205@hanmail.net>
2022-07-25 15:39:43 +0900

    [22.07.25] 자바의 정석 기초 - 7장 연습문제 풀이

--- javajungsuk_basic/src/Practice7/practiceProblem/Ex7_2.java
+++ javajungsuk_basic/src/Practice7/practiceProblem/Ex7_2.java
@@ -7,10 +7,14 @@ class SutdaDeck2 {
 
     SutdaDeck2() {
         //7_1 정답
-        for (int i = 0; i < cards.length; i++) {
-            int num = i % 10 + 1;
+        for (int i = 0; i < cards.length; i++) { //cards의 크기만큼 반복 수행
+//            0~19까지의 숫자가 변하는 동안 원하는 num의 값을 얻으려면, i % 10 + 1 계산식을 사용한다.
+            int num = i % 10 + 1; // 0~9까지. 카드는 1부터 시작이기때문에 +1을 해준다.
+          /*  20장의 카드 -> 1~10까지 카드 한쌍씩 있고 숫자가 1, 3, 8 인경우 둘중의
+              한 장은 K이어야 한다. */
             boolean isKwang = (i < 10) && (num == 1 || num == 3 || num == 8);
 
+            //cards배열안에 각각 숫자와 true&false값을 저장한다.
             cards[i] = new SutdaCard2(num, isKwang);
 
         }
